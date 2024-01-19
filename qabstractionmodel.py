2067b9fb50e5851f296d909d7db473cd165bbe79
BeachyNature <43916527+BeachyNature@users.noreply.github.com>
2024-01-19 15:16:08 -0500

    got multi-table working

--- qabstractionmodel.py
+++ qabstractionmodel.py
@@ -1,8 +1,8 @@
 import sys
 import pandas as pd
 from PyQt5.QtCore import Qt, QAbstractTableModel
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTableView, QCheckBox, QScrollArea, QTabWidget, QAbstractSlider
-
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QTableView, QCheckBox, QScrollArea, QTabWidget, QSplitter
 
 class DataFrameTableModel(QAbstractTableModel):
     def __init__(self, dataframe, column_checkboxes, parent=None):
@@ -49,18 +49,31 @@ class DataFrameTableModel(QAbstractTableModel):
         self.beginInsertRows(index, self.visible_rows, self.visible_rows + rows_to_fetch - 1)
         self.visible_rows += rows_to_fetch
         self.endInsertRows()
+    
+    def get_dataframe(self):
+        visible_columns = [col for col, checkbox in self.column_checkboxes.items() if checkbox.isChecked()]
+        return pd.DataFrame(self.dataframe[visible_columns])
+
 
 class ExpandableText(QWidget):
-    def __init__(self, dataframe, csv_name, tab_widget):
+    def __init__(self, dataframe, csv_name, tab_widget, index, splitter, dict):
         super().__init__()
 
         self.is_expanded = False
+        self.first_split = False
         self.dataframe = dataframe
         self.csv_name = csv_name
         self.tab_widget = tab_widget
+        self.splitter = splitter
+        self.dict = dict
+        self.index = index
+
         self.column_checkboxes = self.create_column_checkboxes()
 
-        self.init_ui()
+        if isinstance(self.index, int):
+            self.setup_data()
+        else:
+            self.init_ui()
 
     def create_column_checkboxes(self):
         column_checkboxes = {}
@@ -68,145 +81,163 @@ class ExpandableText(QWidget):
             checkbox = QCheckBox(column)
             checkbox.setChecked(True)
             checkbox.setVisible(self.is_expanded)
-            checkbox.stateChanged.connect(self.update_table_content)
+            checkbox.stateChanged.connect(self.setup_data)
             column_checkboxes[column] = checkbox
         return column_checkboxes
 
     def init_ui(self):
-        layout = QHBoxLayout()
 
-        labels_layout = QVBoxLayout()
-        label_text = f'<a href="#">{self.csv_name}</a>'
-        self.label = QLabel(label_text)
-        self.label.linkActivated.connect(self.toggle_expansion)
-        labels_layout.addWidget(self.label, alignment=Qt.AlignTop)
+        # Layouts
+        layout = QHBoxLayout()
+        button_layout = QVBoxLayout()
+
+        # Button setups
+        self.check_button = QPushButton(self.csv_name + " +")
+        if not self.dataframe.empty:
+            self.check_button.setStyleSheet('border: none; color: black; font-size: 24px;')
+        else:
+            self.check_button.setStyleSheet('border: none; color: red; font-size: 24px;')
+
+        self.check_button.clicked.connect(self.toggle_expansion)
+        button_layout.addWidget(self.check_button, alignment=Qt.AlignTop)
 
         self.options_widget = QWidget()
         options_layout = QVBoxLayout(self.options_widget)
         for checkbox in self.column_checkboxes.values():
             options_layout.addWidget(checkbox)
 
-        labels_layout.addWidget(self.options_widget)
-        layout.addLayout(labels_layout)
+        button_layout.addWidget(self.options_widget)
+        layout.addLayout(button_layout)
         layout.addStretch()
-
-        self.symbol_button = QPushButton("+")
-        self.symbol_button.setFixedSize(20, 20)
-        self.symbol_button.clicked.connect(self.toggle_expansion)
-        layout.addWidget(self.symbol_button)
-
         self.setLayout(layout)
 
     def toggle_expansion(self):
         self.is_expanded = not self.is_expanded
-        self.symbol_button.setText("-" if self.is_expanded else "+")
+        self.check_button.setText(self.csv_name + " -" if self.is_expanded else  self.csv_name + " +")
 
         for i in range(self.options_widget.layout().count()):
             option_widget = self.options_widget.layout().itemAt(i).widget()
             option_widget.setVisible(self.is_expanded)
 
-        self.update_table_content()
-
-    def update_table_content(self):
-        tab_name = self.csv_name
-        if tab_name not in self.tab_widget.tab_dict:
-            model = DataFrameTableModel(self.dataframe, self.column_checkboxes)
-            tab = QTableView()
-            tab.setModel(model)
-            self.tab_widget.addTab(tab, tab_name)
-            self.tab_widget.tab_dict[tab_name] = tab
-
-            tab.verticalScrollBar().valueChanged.connect(self.load_more_data)
-
-            for checkbox in self.column_checkboxes.values():
-                checkbox.stateChanged.connect(model.update_visible_columns)
+        self.setup_data()
 
     def load_more_data(self):
         tab = self.tab_widget.tab_dict[self.csv_name]
         model = tab.model()
 
         if tab.verticalScrollBar().value() == tab.verticalScrollBar().maximum():
-            model.fetch_more_data()
-            model.update_visible_rows()
+            if len(self.dataframe) > 100:
+                model.fetchMore()
+                model.update_visible_rows()
 
-from PyQt5.QtWidgets import QTabWidget, QSplitter
+    def setup_data(self):
+        tab_name = self.csv_name
+        if tab_name not in self.tab_widget.tab_dict:
+            model = DataFrameTableModel(self.dataframe, self.column_checkboxes)
+            table = QTableView()
+            table.setModel(model)
+
+            self.dict[table] = model
+
+            self.tab_widget.addTab(table, self.csv_name)
+            self.tab_widget.tab_dict[self.csv_name] = table
+
+            # Initial split: add the new tab widget to the QSplitter
+            if isinstance(self.index, int):
+                if not self.first_split:
+                    self.first_split = True
+                    self.splitter.insertWidget(1, self.tab_widget)
+                else:
+                    # Subsequent double-taps: add the new tab widget to the initially split tab widget
+                    self.splitter.widget(1).addTab(table, self.csv_name)
+            
+            for i, j in self.dict.items():
+                i.verticalScrollBar().valueChanged.connect(self.load_more_data)
+                self.check_status(j)
+
+    def check_status(self, model):
+        for checkbox in self.column_checkboxes.values():
+            checkbox.stateChanged.connect(model.update_visible_columns)
+
+
 
 class DataFrameViewer(QWidget):
     def __init__(self, data):
         super().__init__()
-
+        self.incr = 0
         self.init_ui(data)
 
     def init_ui(self, data):
-        main_layout = QVBoxLayout()
+        
+        # Setup Layouts
+        self.main_layout = QVBoxLayout()
+        labels_layout = QVBoxLayout()
+        self.splitter = QSplitter(Qt.Horizontal)
 
-        self.splitter = QSplitter(Qt.Horizontal)  # Use QSplitter to arrange tab widgets side by side
-
+        # Setup Widgets
         self.tab_widget = QTabWidget()
+        scroll_widget = QWidget()
+        scroll_area = QScrollArea()
+
+        # Setup tab dictionary
+        self.model_dict = {}
         self.tab_widget.tab_dict = {}
 
-        scroll_area = QScrollArea()
-        labels_layout = QVBoxLayout()
-
-        # df1 = pd.DataFrame({
-        #     'A': range(1, 1001),
-        #     'B': range(1001, 2001),
-        #     'C': range(2001, 3001),
-        #     'D': range(3001, 4001),
-        #     'E': range(4001, 5001)
-        # })
-        # df1.name = 'Large DataFrame 1'
-
-        # df2 = pd.DataFrame({
-        #     'X': range(5001, 6001),
-        #     'Y': range(6001, 7001),
-        #     'Z': range(7001, 8001),
-        #     'W': range(8001, 9001),
-        #     'V': range(9001, 10001)
-        # })
-        # df2.name = 'Large DataFrame 2'
-
+        # Run the data through the expanded text list
         for csv_name, df in data.items():
-            text_widget = ExpandableText(df, csv_name, self.tab_widget)
+            text_widget = ExpandableText(df, csv_name, self.tab_widget, None, self.splitter, self.model_dict)
             labels_layout.addWidget(text_widget)
 
-        scroll_widget = QWidget()
+        # Configure layouts
         scroll_widget.setLayout(labels_layout)
         scroll_area.setWidget(scroll_widget)
         scroll_area.setWidgetResizable(True)
-
-        main_layout.addWidget(scroll_area)
-
+        self.main_layout.addWidget(scroll_area)
         self.splitter.addWidget(self.tab_widget)
-        self.splitter.addWidget(QTabWidget())  # Initially add an empty tab widget
-        main_layout.addWidget(self.splitter)
+        self.main_layout.addWidget(self.splitter)
+        self.setLayout(self.main_layout)
 
-        self.setLayout(main_layout)
-
+        # Tab widget, double click to compare and able to be removed
+        self.tab_widget.setMovable(True)
+        self.tab_widget.setTabsClosable(True)
         self.tab_widget.tabBarDoubleClicked.connect(self.load_table_double_click)
+    
+    def get_current_tab_dataframe(self):
+        current_index = self.tab_widget.currentIndex()
+        if current_index != -1:
+            current_tab = self.tab_widget.widget(current_index)
+            if isinstance(current_tab, QTableView):
+                model = current_tab.model()
+                if model:
+                    return model.get_dataframe()
+        return pd.DataFrame()  # Return an empty DataFrame if no data is available
 
     def load_table_double_click(self, index):
-        tab_name = self.tab_widget.tabText(index)
-        if tab_name in self.tab_widget.tab_dict:
-            model = DataFrameTableModel(self.tab_widget.tab_dict[tab_name].model().dataframe,
-                                        self.tab_widget.tab_dict[tab_name].model().column_checkboxes)
-            tab = QTableView()
-            tab.setModel(model)
 
-            # Create a new tab widget and add the tab to it
-            new_tab_widget = QTabWidget()
-            new_tab_widget.addTab(tab, tab_name + "_Duplicate")
-
-            # Get the index of the old tab widget
-            old_tab_widget_index = self.splitter.indexOf(self.tab_widget)
-
-            # Remove the tab from the old tab widget
+        # Remove the tab that you want to switch over
+        if self.tab_widget.count() > 1:
+            self.incr += 1
+            tab_name = self.tab_widget.tabText(index)
+            self.tab_widget.tab_dict.pop(tab_name)
             self.tab_widget.removeTab(index)
 
-            # If there are no more tabs in the old tab widget, remove the old tab widget itself
-            if self.tab_widget.count() == 0:
-                self.splitter.removeWidget(self.tab_widget)
+            # Create new tab
+            new_tab_widget = QTabWidget()
+            new_tab_widget.setTabsClosable(True)
+            new_tab = tab_name + "-" + str(self.incr)
+            new_tab_widget.tab_dict = {}
+            dataframe = self.get_current_tab_dataframe()
 
-            # Add the new tab widget to the QSplitter at the same index as the old tab widget
-            self.splitter.insertWidget(old_tab_widget_index, new_tab_widget)
+            if new_tab_widget.count() == 0:
+                new_tab_widget.tabCloseRequested.connect(self.tabCloseRequested)
+                # dummy_widget = QWidget()
+                # self.splitter.replaceWidget(new_tab_widget, dummy_widget)
 
+            if not dataframe.empty:
+                ExpandableText(dataframe, new_tab, new_tab_widget, index, self.splitter, self.model_dict)
+            else:
+                print("Cannot Compare, dataframe is empty!")
+
+    def tabCloseRequested(self, index):
+        if self.tab_widget.count() > 1:
+            self.splitter.widget(1).setParent(None)
